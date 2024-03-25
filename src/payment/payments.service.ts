afa732b405a3d3e8d3aa1b8d9b47b2c79bf43cc0
ZeBolHD <zezebolz@gmail.com>
2024-03-25 11:14:15 +0300

    chore: added logger

--- src/payment/payments.service.ts
+++ src/payment/payments.service.ts
@@ -3,6 +3,7 @@ import {
   Inject,
   Injectable,
   InternalServerErrorException,
+  Logger,
   NotFoundException,
   ServiceUnavailableException,
 } from "@nestjs/common";
@@ -21,17 +22,32 @@ export class PaymentService {
     private readonly prismaService: PrismaService,
   ) {}
 
+  private readonly logger = new Logger(PaymentService.name);
+
   async findAllPayments(dto: FindAllQueryDto) {
-    const payments = await this.prismaService.payment.findMany({
-      where: {
-        status: dto.status,
-      },
-      take: dto.pageSize,
-      skip: dto.page * dto.pageSize - dto.pageSize,
-      orderBy: {
-        createdAt: dto.createdAt,
-      },
-    });
+    this.logger.log("------------------- Finding all payments -------------------");
+
+    this.logger.verbose("Page: " + dto.page);
+    this.logger.verbose("Page size: " + dto.pageSize);
+    this.logger.verbose("Status: " + dto.status);
+    this.logger.verbose("Created at: " + dto.createdAt);
+
+    const payments = await this.prismaService.payment
+      .findMany({
+        where: {
+          status: dto.status,
+        },
+        take: dto.pageSize,
+        skip: dto.page * dto.pageSize - dto.pageSize,
+        orderBy: {
+          createdAt: dto.createdAt,
+        },
+      })
+      .catch(() => {
+        this.logger.error("Database error");
+        this.logger.log("------------------- End finding all payments -------------------");
+        throw new ServiceUnavailableException("Сервис недоступен");
+      });
 
     const count = await this.prismaService.payment
       .aggregate({
@@ -45,43 +61,98 @@ export class PaymentService {
 
     const pageCount = Math.ceil(count / dto.pageSize);
 
+    this.logger.log("Found data:");
+
+    this.logger.verbose("Count: " + count);
+    this.logger.verbose("Page count: " + pageCount);
+    this.logger.verbose("Page: " + dto.page);
+    this.logger.verbose("Page size: " + dto.pageSize);
+
+    this.logger.log("------------------- End finding all payments -------------------");
+
     return { payments, count, page: dto.page, pageSize: dto.pageSize, pageCount };
   }
 
   async findAllUserPaymentsByUserId(userId: string) {
-    const user = await this.prismaService.user.findUnique({
-      where: {
-        id: userId,
-      },
-      select: {
-        id: true,
-      },
-    });
+    this.logger.log("------------------- Finding all payments by userId -------------------");
+
+    this.logger.verbose("User ID: " + userId);
+
+    const user = await this.prismaService.user
+      .findUnique({
+        where: {
+          id: userId,
+        },
+        select: {
+          id: true,
+        },
+      })
+      .catch(() => {
+        this.logger.error("Database error");
+        this.logger.log(
+          "------------------- End finding all payments by userId -------------------",
+        );
+        throw new ServiceUnavailableException("Сервис недоступен");
+      });
 
     if (!user) {
+      this.logger.error("User not found");
+      this.logger.log("------------------- End finding all payments by userId -------------------");
       throw new NotFoundException("Пользователь не найден");
     }
 
-    const payments = await this.prismaService.payment.findMany({
-      where: {
-        userId,
-      },
-    });
+    const payments = await this.prismaService.payment
+      .findMany({
+        where: {
+          userId,
+        },
+      })
+      .catch(() => {
+        this.logger.error("Database error");
+        this.logger.log(
+          "------------------- End finding all payments by userId -------------------",
+        );
+        throw new ServiceUnavailableException("Сервис недоступен");
+      });
+
+    this.logger.verbose("Found data: " + payments.length);
+
+    this.logger.log("------------------- End finding all payments by userId -------------------");
 
     return payments;
   }
 
   async findAllUserPayments(userId: string) {
-    const payments = await this.prismaService.payment.findMany({
-      where: {
-        userId,
-      },
-    });
+    this.logger.log("------------------- Finding all user payments -------------------");
+
+    this.logger.verbose("User ID: " + userId);
+
+    const payments = await this.prismaService.payment
+      .findMany({
+        where: {
+          userId,
+        },
+      })
+      .catch(() => {
+        this.logger.error("Database error");
+        this.logger.log("------------------- End finding all user payments -------------------");
+        throw new ServiceUnavailableException("Сервис недоступен");
+      });
+
+    this.logger.verbose("Found data: " + payments.length);
+
+    this.logger.log("------------------- End finding all user payments -------------------");
 
     return payments;
   }
 
   async createPayment(userId: string, dto: CreatePaymentDto) {
+    this.logger.log("------------------- Creating payment -------------------");
+
+    this.logger.verbose("User ID: " + userId);
+    this.logger.verbose("Movie ID: " + dto.movieId);
+    this.logger.verbose("Amount: " + dto.amount);
+
     const user = await this.prismaService.user.findUnique({
       where: {
         id: userId,
@@ -92,6 +163,8 @@ export class PaymentService {
     });
 
     if (!user) {
+      this.logger.error("User not found");
+      this.logger.log("------------------- End creating payment -------------------");
       throw new NotFoundException("Пользователь не найден");
     }
 
@@ -106,10 +179,13 @@ export class PaymentService {
         },
       })
       .catch(() => {
+        this.logger.error("Database error");
         throw new NotFoundException("Фильм не найден");
       });
 
     if (!movie) {
+      this.logger.error("Movie not found");
+      this.logger.log("------------------- End creating payment -------------------");
       throw new NotFoundException("Фильм не найден");
     }
 
@@ -120,14 +196,20 @@ export class PaymentService {
       card: dto.card,
     };
 
+    this.logger.log("Sending data to card checker...");
+
     const cardCheckerResponse = await this.sendMessageWithTimeoutError<
       CardCheckerResponse,
       CardCheckerDto
     >(this.checkerClient, "create.payment", cardCheckerDto).catch(() => {
+      this.logger.error("Card checker unavailable");
+      this.logger.log("------------------- End creating payment -------------------");
       throw new ServiceUnavailableException("Сервис карт временно недоступен");
     });
 
     const cardCheckerStatus = cardCheckerResponse.status;
+
+    this.logger.verbose("Card checker status: " + cardCheckerStatus);
 
     const paymentRegistryDto: PaymentRegistryDto = {
       userId,
@@ -137,16 +219,26 @@ export class PaymentService {
       status: cardCheckerStatus,
     };
 
+    this.logger.log("Sending data to payment registry...");
+
     const paymentRegistryResponse = await this.sendMessageWithTimeoutError<
       PaymentRegistryResponse,
       PaymentRegistryDto
     >(this.registryClient, "register.payment", paymentRegistryDto).catch(() => {
+      this.logger.error("Payment registry unavailable");
+      this.logger.log("------------------- End creating payment -------------------");
       throw new ServiceUnavailableException("Сервис регистрации платежа временно недоступен");
     });
 
     if (!paymentRegistryResponse) {
+      this.logger.error("Payment registry error");
+      this.logger.log("------------------- End creating payment -------------------");
       throw new BadRequestException("Произошла ошибка при регистрации платежа");
     }
+
+    this.logger.verbose("Payment registry status: " + paymentRegistryResponse.status);
+
+    this.logger.log("------------------- End creating payment -------------------");
 
     switch (cardCheckerStatus) {
       case Status.SUCCESS:
